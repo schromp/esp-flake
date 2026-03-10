bf07bd4e122633003cb339ea4c9c2f309ed89f38
schromp <schromp@echsen.club>
2026-03-10 23:39:30 +0100

    Revert "update test"
    This reverts commit e018f7536084c3fde8fc99d8d2290cb2052befc7.

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "ESP32 & Rust";
 
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-25.11";
+    nixpkgs.url = "nixpkgs/nixos-24.11";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
